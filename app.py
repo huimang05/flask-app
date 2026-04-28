45e19c9326d2b2f788d3f43dff7fd0d789c02994
huimang05 <tanisha.londhe@aissmsioit.org>
2026-04-28 10:43:40 +0530

    Initial commit: Flask app setup

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-@app.route('/login')
-def login():
-    return "Login Page"
-app.run(debug=False, port=5001)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Flask!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
