e9d2e5d75620317cbef056d1851d79f0532e6db6
huimang05 <tanisha.londhe@aissmsioit.org>
2026-04-28 09:29:36 +0530

    Add login route and template

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Hello, Flask!"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/login')
+def login():
+    return "Login Page"
